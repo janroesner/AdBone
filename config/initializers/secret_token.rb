d22c085614ad1e148d6dd24e813eee9a851c6b82
Jan Roesner <jan@roesner.it>
2011-03-16 22:34:05 +0100

    Initial commit. Signed-off-by: Jan Roesner <jan@roesner.it>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adbone::Application.config.secret_token = 'deedb73166d829473f6b2eb6e0287399e63ac769ae7b97db174cd7c8dc2b4d52a489c8695633b5879d1b2ed249307ecfb18065fba633992f90353bf730f67bab'
