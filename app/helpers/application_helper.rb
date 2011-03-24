5088cadc899141fe5ad664d95b9e6f1ec81820ea
Jan Roesner <jan@roesner.it>
2011-03-24 11:03:11 +0100

    Slightly refactored ich_templates before bigger changes. Signed-off-by: Jan Roesner <jan@roesner.it>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,20 @@
 module ApplicationHelper
 
+  # returns all ich templates for given models
   def ich_templates *models
     templates = []
     models.each do |model|
-      Dir.new(File.dirname(Rails.root.to_s+"/app/views/#{model.to_s}/*")).entries.delete_if{|n| n.match /^\./}.collect {|fn| [fn,File.open(Rails.root.to_s+"/app/views/#{model.to_s}/"+fn){ |f| f.read }] }.collect{|ary| ary.last}.each do |template|
-        haml_engine = Haml::Engine.new(template)
-        templates << haml_engine.render
+      Dir.new(File.dirname(Rails.root.to_s+"/app/views/#{model.to_s}/*")).entries.delete_if do |file|
+        file.match /^\./
+      end.collect do |filename|
+          [filename ,File.open(Rails.root.to_s+"/app/views/#{model.to_s}/" + filename) do |file|
+            file.read
+          end]
+      end.collect{|ary| ary.last}.each do |template|
+        templates << Haml::Engine.new(template).render
       end
     end
-    templates.join
+    templates.join.html_safe
   end
 
 end
